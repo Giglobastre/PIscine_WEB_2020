0b3122a9341eeb3acc3de1576ff3f0791ad4432c
Kenny HUBER <36986213+Giglobastre@users.noreply.github.com>
2020-04-12 15:32:21 +0200

    bdd util + obj avec les admin

---
+++ BDD/objets.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  Dim 12 avr. 2020 à 13:31
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `pj_web2020`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `objets`
+--
+
+DROP TABLE IF EXISTS `objets`;
+CREATE TABLE IF NOT EXISTS `objets` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(255) NOT NULL,
+  `Photo1` varchar(255) NOT NULL,
+  `Photo2` varchar(255) NOT NULL,
+  `Photo3` varchar(255) NOT NULL,
+  `Video` varchar(255) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Prix` int(11) NOT NULL,
+  `Catégorie` int(11) NOT NULL,
+  `Date` datetime NOT NULL DEFAULT current_timestamp(),
+  `ID_V` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
