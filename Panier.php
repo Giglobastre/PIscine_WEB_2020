86328de2da0a73fafc57b9d8dfe1f22ce8146dd5
HugoTeinturier <47658460+HugoTeinturier@users.noreply.github.com>
2020-04-15 14:35:23 +0200

    Add files via upload

--- Panier.php
+++ Panier.php
@@ -1,17 +1,21 @@
+<?php 
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="utf-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="icon" type="image/jpg" href="logo%20Ebay%20ECE.JPG" />
+  <link rel="icon" type="image/jpg" href="../Images/logo%20Ebay%20ECE.JPG" />
   <link rel="stylesheet" type="text/css" href="../Style/Style_panier.css">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> </script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
 </head>
 <body>
-  
+
   <div id="header">
     <img src="../Images/logo%20Ebay%20ECE.JPG" height="40" width="160"> 
   </div>
@@ -19,8 +23,9 @@
   <div class="topnav">
     <div style="float:left">
       <a href="index.php">Home</a>
-      <a href="news.php">News</a>
-      <a href="Produit.php">Produit</a>
+      <a href="Enchères_en_cours.php">Encherères en cours</a>
+      <a href="Achats_immédiats.php">Achats immédiats</a>
+      <a href="Négociation.php">Négociation</a>
       <a href="Contact.php">Contact</a>
       <a href="about.php">About</a>
     </div>
@@ -30,71 +35,68 @@
     </div>     
   </div>   
 
-<?php 
-session_start();
-
-$_SESSION['ID_Client']=1;
-$total=0;
-
-
-
-$condition=0;
-try{
-  $bdd = new PDO('mysql:host=localhost;dbname=pj_web2020;charset=utf8', 'root', '');
-
-  $req = $bdd->prepare("SELECT ID_Client ,ID_Objet FROM panier"); 
-  $req->execute();
+  <?php 
   
-  while ($data = $req->fetch()){
 
-    if($data['ID_Client']==$_SESSION['ID_Client'])
-    {
-      $condition=1;
-      echo ("id client :");
-      echo '<h3>'.$data['ID_Client'].'</h3>';
-      echo ("id object :");
-      echo '<h3>'.$data['ID_Objet'].'</h3>';
-      $req2 = $bdd->prepare("SELECT ID,Prix FROM objets WHERE ID='".$data['ID_Objet']."'"); 
-      $req2->execute();
-      
-      while ($data = $req2->fetch())
+  $_SESSION['ID_Client'];
+  $total=0;
+
+
+
+  $condition=0;
+  try{
+    $bdd = new PDO('mysql:host=localhost;dbname=pj_web2020;charset=utf8', 'root', '');
+
+    $req = $bdd->prepare("SELECT ID_Client ,ID_Objet,Acquereur,ID_transac FROM panier"); 
+    $req->execute();
+
+    while ($data = $req->fetch()){
+
+      if(($data['ID_Client']==$_SESSION['ID_Client'])&&($data['Acquereur']==1))
       {
-        $total=$data['Prix']+$total;
-        echo ("Prix :");
-        echo '<h3>'.$data['Prix'].'</h3>';
-        
+        $condition=1;
+        echo ("id client :");
+        echo '<h3>'.$data['ID_transac'].'</h3>';
+        echo ("id object :");
+        echo '<h3>'.$data['ID_Objet'].'</h3>';
+        $req2 = $bdd->prepare("SELECT ID,Prix FROM objets WHERE ID='".$data['ID_Objet']."'"); 
+        $req2->execute();
+
+        while ($data = $req2->fetch())
+        {
+          $total=$data['Prix']+$total;
+          echo ("Prix :");
+          echo '<h3>'.$data['Prix'].'</h3>';
+
+        }
+
+
+
+        $req2->closeCursor();;
       }
 
-      
 
-      $req2->closeCursor();;
+    }
+    echo("Total panier =");
+    echo $total;
+    if($condition==0)
+    {
+      echo("panier vide");
     }
 
-    
+
+    $req->closeCursor();;
   }
-  echo("Total panier =");
-  echo $total;
-  if($condition==0)
-  {
-    echo("panier vide");
+  catch (Exception $e){
+
+    die('Erreur : ' . $e->getMessage());
   }
-  
 
-  $req->closeCursor();;
-}
-catch (Exception $e){
-  
-  die('Erreur : ' . $e->getMessage());
-}
-
-?> 
-
-    
-<br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+  ?>
+  <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
   <div id="footer">
-    Copiright &copy; 2020; 
+    Copyright &copy; 2020; 
     Clément Viéville - Hugo Teinturier - Kenny Huber
   </div>
-
 </body>
 </html>
