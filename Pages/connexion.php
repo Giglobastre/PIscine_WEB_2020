3e801d839f2958a8a437729a25bd1846c77b8618
wmb988 <63539265+wmb988@users.noreply.github.com>
2020-04-16 10:34:53 +0200

    Add files via upload

--- Pages/connexion.php
+++ Pages/connexion.php
@@ -43,37 +43,61 @@
         
     <form action="../Traitement/Traitement_connexion.php" method="POST">   
         
-        <div style="padding-left:250px">
+        <div style="padding-left:350px">
         <table>
-            <tr><h2><b>Connexion</b></h2></tr>
             <tr>
-            <td><input type="email" name="mail" placeholder="Entrez votre Addresse mail "></td>
-            <td><input type="email" name="mail2" placeholder="Entrez votre Addresse mail "></td 
+                <td><h2><b>Inscription ECEbay</b></h2></td>
+                <td><h2><b>Connexion</b></h2></td>
+            </tr>
+            <tr>
+            <td> <input type="text" name="nom" placeholder="Entrez votre Nom "></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="prenom" placeholder="Entrez votre Prenom "></td>
+            </tr>
+            <tr>
+                <td> <input type="text" name="pseudo" placeholder="Entrez votre Pseudo "></td>
             </tr>
             
             <tr>
-                
-            <td><input type="text" name="mdp" placeholder="Entrez votre Mot de Passe ">  </td>
-                
+                <td><input type="email" name="mail" placeholder="Entrez votre Addresse mail "></td>
+                <td><input type="email" name="mail2" placeholder="Entrez votre Addresse mail "></td 
+            </tr>
+             <br>
+            <tr>                
+                <td><input type="text" name="mdp" placeholder="Entrez votre Mot de Passe ">  </td>
+                <td><input type="text" name="mdp1" placeholder="Entrez votre Mot de Passe ">  </td>
+      
+            </tr>
+             <br>
+            <tr></tr>
+            <tr>  
+                <td><input type="text" name="mdp2" placeholder="Confirmez votre Mot de Passe ">  </td>
+      
+            </tr>
+                    
+            <tr>    
+                <td><b><input type="submit" name="button1" value="Inscription"></b></td>
+                <td><b><input type="submit" name="button2" value="Connexion"></b></td>
+            </tr>
+                    
+                    
+            <tr>
+                <td style="  width:500px";></td> 
             </tr>
             
-            <tr>
-                
-            <td><input type="submit" name="button1" value="submit"></td>
-  
-                
+            <tr>    
+                <td > Acheteur <input type="checkbox" name="formSubmit" value="Acheteur" </b></td>
             </tr>
-                     
-            <br><br>
+                                
+            <tr>    
+                <td > Vendeur <input type="checkbox" name="formSubmit" value="Vendeur" </td>
+            </tr>
+                    
         </table>
-            </div>
+        </div>                
     </form>  
-        
-        
-        
-        
-    
-    <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+   <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
     
     <div id="footer">
       Copyright &copy; 2020; 
